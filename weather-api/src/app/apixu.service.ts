46867ec9dbdffcac7bff49cc76282959266a48bf
Shubham Samantaray <samantaray.shubham423@gmail.com>
2022-09-19 23:18:59 +0530

    updated weather app componet

--- weather-api/src/app/apixu.service.ts
+++ weather-api/src/app/apixu.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApixuService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getWeather(location: string) {
+    return this.http.get('http://api.weatherstack.com/current?access_key=ddbfacf46865e6c8f35302d453e35a33&query=' + location);
+  }
 }
--- weather-api/src/app/weather-app/weather-app.component.ts
+++ weather-api/src/app/weather-app/weather-app.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApixuService } from '../apixu.service';
 
 @Component({
   selector: 'app-weather-app',
   templateUrl: './weather-app.component.html',
   styleUrls: ['./weather-app.component.css']
 })
+
+
 export class WeatherAppComponent implements OnInit {
   public weatherSearch!: FormGroup;
+  public weatherData: any;
   //weathersearch reference to FormGroup:
 
   // By adding the formBuilder to the constructor, it creates an instance of 
   // the FormBuilder class, allowing you to use it within your component.
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private apixuService: ApixuService
+
+  ) { }
 
   ngOnInit() {
     this.weatherSearch = this.formBuilder.group({
@@ -20,8 +28,11 @@ export class WeatherAppComponent implements OnInit {
     });
   }
 
-  sendToAPIXU(formValues) {
-    console.log(formValues);
+  sendToAPIXU(formValues: any) {
+    this.apixuService.getWeather(formValues.location).subscribe(data => {
+      this.weatherData = data;
+      console.log(this.weatherData);
+    });
   }
 
 }
